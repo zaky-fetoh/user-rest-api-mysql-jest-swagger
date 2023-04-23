46b7e2caf579bd119536e92948e716800e529fbe
Zaky fetoh <zaky.fetoh@gmail.com>
2023-04-23 14:34:22 +0200

    controller deleteUser is added it accept User Id and delte the corresonding record from DB

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,21 +1,36 @@
-const {usersModel} = require("../model/users")
+const { usersModel } = require("../model/users")
 
 
-exports.addUser = async(req, res, next)=>{
-    try{
-    const user  = req.body
-    const userObjSavedtoDB = await usersModel.create(user);
-    res.status(200).json({
-        ok:true, message:"user added", 
-        data:{
-            userId: userObjSavedtoDB.id,
-        }
-    })
-    }catch(e){
+exports.addUser = async (req, res, next) => {
+    /***
+     * URL: /user  
+     * Method: POST
+     * input: POST body JSON File according user schema
+     * It addes a user object to tthe database 
+     */
+    try {
+        const user = req.body
+        const userObjSavedtoDB = await usersModel.create(user);
+        res.status(200).json({
+            ok: true, message: "user added",
+            data: {
+                userId: userObjSavedtoDB.id,
+            }
+        })
+    } catch (e) {
         res.status(500).json({
-            ok:false, message:e.message,
+            ok: false, message: e.message,
         })
     }
 }
 
-exports.
+exports.deleteUser = async (req, res, next) => {
+    /***
+     * url: /user/:userId
+     * method: DELETE
+     * input: params user Id
+     */
+    try{const userId = req.params.userId; 
+    const user = await usersModel.findByPk(userId);
+    await user.destroy();
+    res.status(200).json({
