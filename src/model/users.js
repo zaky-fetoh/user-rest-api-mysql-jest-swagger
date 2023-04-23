8229a9721ba64e7a3e08fc01cb82438aa4ce8c92
Zaky fetoh <zaky.fetoh@gmail.com>
2023-04-23 14:13:49 +0200

    users model validation schema joi is added to validate after creation

--- src/model/users.js
+++ src/model/users.js
@@ -3,28 +3,40 @@ const joi = require("joi");
 
 const database = require("../utils/database");
 
-const USER_TABLE_NAME  = "users";
+const USER_TABLE_NAME = "users";
 
-const userModel = database.define(USER_TABLE_NAME,{
-    id:{
-        type:sequelize.DataTypes.INTEGER,
-        autoIncrement: true, 
-        allowNull: false, 
+const usersModel = database.define(USER_TABLE_NAME, {
+    id: {
+        type: sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
         primaryKey: true,
     },
-    name:{
-        type:sequelize.DataTypes.STRING, 
-        allowNull:false, 
+    name: {
+        type: sequelize.DataTypes.STRING(25),
+        allowNull: false,
     },
-    phone:{
+    phone: {
+        type: sequelize.DataTypes.STRING(20),
+    },
+    email: {
         type: sequelize.DataTypes.STRING,
-    },
-    email:{
-        type:sequelize.DataTypes.STRING,
     }
 });
 
 
-const joiValidator = joi.object({
-    
+const joiUserValidator = joi.object({
+    id: joi.any(),
+    name: joi.string().min(4).max(15).required(),
+    email: joi.string().email().required(),
+    phone: joi.string().length(12),
 })
+
+
+
+usersModel.afterCreate(user => {
+    const { error } = joiUserValidator(user);
+    if (error) throw Error(error)
+})
+
+module.exports = {USER_TABLE_NAME, usersModel}
