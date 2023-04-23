c22cac2132f2ec27eca5f32513c578c6ad86768e
Zaky fetoh <zaky.fetoh@gmail.com>
2023-04-23 12:52:13 +0200

    users model schema is added such that it has id name phone and emil

--- src/model/users.js
+++ src/model/users.js
@@ -1,1 +1,30 @@
-const database = require("../utils/database")
+const sequelize = require("sequelize");
+const joi = require("joi");
+
+const database = require("../utils/database");
+
+const USER_TABLE_NAME  = "users";
+
+const userModel = database.define(USER_TABLE_NAME,{
+    id:{
+        type:sequelize.DataTypes.INTEGER,
+        autoIncrement: true, 
+        allowNull: false, 
+        primaryKey: true,
+    },
+    name:{
+        type:sequelize.DataTypes.STRING, 
+        allowNull:false, 
+    },
+    phone:{
+        type: sequelize.DataTypes.STRING,
+    },
+    email:{
+        type:sequelize.DataTypes.STRING,
+    }
+});
+
+
+const joiValidator = joi.object({
+    
+})
