8af9c138fe0ec9a4561b8a5ab42f33b4da0f0c48
Zaky fetoh <zaky.fetoh@gmail.com>
2023-04-24 10:24:34 +0200

    delete test unit is add

--- src/controller/users.test.js
+++ src/controller/users.test.js
@@ -1,68 +1,127 @@
 
 
-describe("user Controller Function test",()=>{
+describe("user Controller Function test", () => {
     beforeEach(() => {
         jest.resetModules();
     });
-    
-    it("should insert", ()=>{
+
+    it("should insert", () => {
         const ID_RETURNED_BY_DB = 10
         const reqMock = {
-            body:{
-                name:"mahmoud", 
-                phone:"1234567910", 
-                email:"hello@gmail.com", 
+            body: {
+                name: "mahmoud",
+                phone: "1234567910",
+                email: "hello@gmail.com",
             }
         };
         const resMock = {
-            status: jest.fn((x)=>{
+            status: jest.fn((x) => {
                 expect(x).toBe(200);
                 return resMock
             }),
-            json:jest.fn((x)=>{
+            json: jest.fn((x) => {
                 expect(x.data.userId).toBe(ID_RETURNED_BY_DB);
                 expect(x.ok).toBe(true);
             })
         }
-        jest.doMock("../model/users", ()=>({
+        jest.doMock("../model/users", () => ({
             usersModel: {
-                create: jest.fn(async(x)=>{
+                create: jest.fn(async (x) => {
                     expect(x.name).toBe("mahmoud");
                     expect(x.phone).toBe("1234567910");
                     expect(x.email).toBe("hello@gmail.com");
-                    return{id: ID_RETURNED_BY_DB}
+                    return { id: ID_RETURNED_BY_DB }
                 })
             }
         }))
         require("./users").addUser(reqMock, resMock)
     })
 
-    it("should not insert", ()=>{
+    it("should not insert", () => {
         const EXAMPLE_ERROR = "MockedError"
-        jest.doMock("../model/users", ()=>({
+        jest.doMock("../model/users", () => ({
             usersModel: {
-                create: jest.fn(async()=>{
+                create: jest.fn(async () => {
                     throw new Error(EXAMPLE_ERROR);
                 })
             }
         }));
         const reqMock = {
-            body:{
-                name:"hello"
+            body: {
+                name: "hello"
             }
         };
         const resMock = {
-            status: jest.fn((x)=>{
+            status: jest.fn((x) => {
                 expect(x).toBe(500);
                 return resMock
             }),
-            json:jest.fn((x)=>{
+            json: jest.fn((x) => {
                 expect(x.ok).toBe(false);
                 expect(x.message).toBe(EXAMPLE_ERROR)
             })
         }
-        require("./users").addUser(reqMock,resMock)
+        require("./users").addUser(reqMock, resMock)
     })
 
+    it("should Delete", async () => {
+        const searchId = "id";
+        const destroyMock = jest.fn(async () => { })
+        jest.doMock("../model/users", () => ({
+            usersModel: {
+                findByPk: jest.fn(async (x) => {
+                    expect(x).toBe(searchId)
+                    return {
+                        destroy: destroyMock,
+                    }
+                })
+            }
+        }));
+        const reqMock = {
+            params: {
+                userId: searchId
+            }
+        };
+        const resMock = {
+            status: jest.fn((x) => {
+                expect(x).toBe(200);
+                return resMock
+            }),
+            json: jest.fn((x) => {
+                expect(x.deletedId).toBe(searchId);
+                expect(x.ok).toBe(true);
+            })
+        };
+        await require("./users").deleteUser(reqMock, resMock,)
+        expect(destroyMock).toBeCalled()
+    })
+
+    it("should not delete",async()=>{
+        const searchId = "id";
+        jest.doMock("../model/users", () => ({
+            usersModel: {
+                findByPk: jest.fn(async (x) => {
+                    expect(x).toBe(searchId)
+                    return null
+                })
+            }
+        }));
+        const reqMock = {
+            params: {
+                userId: searchId
+            }
+        };
+        const resMock = {
+            status: jest.fn((x) => {
+                expect(x).toBe(500);
+                return resMock
+            }),
+            json: jest.fn((x) => {
+                expect(x.ok).toBe(false);
+                expect(x.message).toBe("user does not Exist")
+            })
+        };
+        await require("./users").deleteUser(reqMock, resMock,)
+    })
 
 })
